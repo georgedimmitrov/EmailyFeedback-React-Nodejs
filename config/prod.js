4aeb02f402525897208c9280ae3d7cbf2de7a3e1
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-17 15:53:18 +0300

    finished google auth flow

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+// 491540891634-347dp5kbgltjuir4kjstbt8uknduvk8e.apps.googleusercontent.com
+// jTS0_kf3PNIYshr6qfE5w4uD
+// mongodb://root:8404@ds145273.mlab.com:45273/emaily-prod-db
