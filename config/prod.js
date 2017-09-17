b7161a3f65282f5277ea6afb25e2e8a48964e102
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-17 16:03:20 +0300

    well

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-// 491540891634-347dp5kbgltjuir4kjstbt8uknduvk8e.apps.googleusercontent.com
-// jTS0_kf3PNIYshr6qfE5w4uD
-// mongodb://root:8404@ds145273.mlab.com:45273/emaily-prod-db
