5b1d716d0587e3e4b79ad486281e114f9d5ad1ed
georgedimmitrov <georgedimmitrov@gmail.com>
2017-09-25 21:17:58 +0300

    production ready build

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // exress will serve up production assets
+  // like main.js file or main.css file
+  app.use(express.static('client/build'));
+
+  // express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.get('/', (req, res) => {
   res.send({ hi: 'hello' });
 });
